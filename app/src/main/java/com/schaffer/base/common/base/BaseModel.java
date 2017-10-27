b8980727df6307730ea471f149600607bae52f18
SchafferWang <a18634804691@gmail.com>
2017-10-27 10:33:21 +0800

    2017年10月27日 10:33:19

--- app/src/main/java/com/schaffer/base/common/base/BaseModel.java
+++ app/src/main/java/com/schaffer/base/common/base/BaseModel.java
@@ -1,162 +1,169 @@
 package com.schaffer.base.common.base;
 
+import com.schaffer.base.BuildConfig;
 import com.schaffer.base.api.ApiInterface;
 import com.schaffer.base.common.utils.LTUtils;
 
+import java.io.IOException;
 import java.lang.reflect.Field;
 import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.concurrent.TimeUnit;
 
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSession;
-import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
- * Created by a7352 on 2017/5/13.
+ * @author SchafferWang
+ * @date 2017/5/13
  */
 
 public abstract class BaseModel<T> {
 
 
-	private T service;
-	private volatile static Retrofit retrofit;
+    private T service;
+    private volatile static Retrofit retrofit;
 
-	public BaseModel() {
-		if (retrofit == null) {
-			synchronized (BaseModel.class) {
-				if (retrofit == null) {
-					OkHttpClient client = new OkHttpClient().newBuilder()
-							.connectTimeout(60, TimeUnit.SECONDS)
-							.readTimeout(60, TimeUnit.SECONDS)
-							.writeTimeout(60, TimeUnit.SECONDS)
-//                            .addInterceptor(createUserAgentInterceptor())
-							.addInterceptor(createHttpLoggingInterceptor())
-//							.addInterceptor(new Interceptor() {//添加头部
-//								@Override
-//								public Response intercept(Chain chain) throws IOException {
-//
-//									Request original = chain.request();
-//
-//									Request request = original.newBuilder()
-//											.header("v", "AppUtils.getVersionCode(BaseApplication.getInstance())")//版本号
-//											.header("sv", Build.MANUFACTURER+" " + Build.VERSION.SDK)//系统版本号
-//											.header("type", "2")//2=android
-//											.method(original.method(), original.body())
-//											.build();
-//
-//									return chain.proceed(request);
-//								}
-//							})
-							.build();
+    public BaseModel() {
+        if (retrofit == null) {
+            synchronized (BaseModel.class) {
+                if (retrofit == null) {
+                    OkHttpClient client = new OkHttpClient().newBuilder()
+                            .connectTimeout(20, TimeUnit.SECONDS)
+                            .readTimeout(20, TimeUnit.SECONDS)
+                            .writeTimeout(20, TimeUnit.SECONDS)
+                            .addInterceptor(createHttpLoggingInterceptor())
+                            .addInterceptor(new MyHeaderInterceptor())
+                            .build();
 
-					if (ApiInterface.HOST_BASE_URL.startsWith("https")) {//https 跳过SSL认证
-						solveHttps(client);
-					}
+                    if (ApiInterface.HOST_BASE_URL.startsWith("https")) {
+                        //https 跳过SSL认证
+                        solveHttps(client);
+                    }
 
-					retrofit = new Retrofit.Builder()
-							.baseUrl(ApiInterface.HOST_BASE_URL)
-							.client(client)
-							.addCallAdapterFactory(RxJavaCallAdapterFactory.create())
-							.addConverterFactory(GsonConverterFactory.create())
-							.build();
-				}
-			}
-		}
-		service = retrofit.create(getServiceClass());
+                    retrofit = new Retrofit.Builder()
+                            .baseUrl(ApiInterface.HOST_BASE_URL)
+                            .client(client)
+                            .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                            .addConverterFactory(GsonConverterFactory.create())
+                            .build();
+                }
+            }
+        }
+        service = retrofit.create(getServiceClass());
 
-	}
+    }
 
 
-	private static Interceptor createHttpLoggingInterceptor() {
-		HttpLoggingInterceptor logging = new HttpLoggingInterceptor(new HttpLoggingInterceptor.Logger() {
-			@Override
-			public void log(String message) {
-				if (message.startsWith("{") && message.endsWith("}")) {
-					LTUtils.w("json:\n" + message + "\n");
-				} else {
-					LTUtils.d(message);
-				}
-			}
-		});
-		logging.setLevel(Boolean.parseBoolean("true") ? HttpLoggingInterceptor.Level.BODY : HttpLoggingInterceptor.Level.NONE);
-		return logging;
-	}
+    private static Interceptor createHttpLoggingInterceptor() {
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor(new MyLogger());
+        logging.setLevel(Boolean.parseBoolean("true") ? HttpLoggingInterceptor.Level.BODY : HttpLoggingInterceptor.Level.NONE);
+        return logging;
+    }
 
 
-	public T getService() {
-		return service;
-	}
+    public T getService() {
+        return service;
+    }
 
-	protected abstract Class<T> getServiceClass();
+    /**
+     * 获取接口定义参数等信息对应的接口
+     *
+     * @return
+     */
+    protected abstract Class<T> getServiceClass();
 
-	//    private static Interceptor createUserAgentInterceptor() {
-//        return new Interceptor() {
-//
-//            @Override
-//            public Response intercept(Chain chain) throws IOException {
-//                Request request = chain.request().newBuilder()
-//                        .header("User-Agent", USER_AGENT)
-//                        .build();
-//                return chain.proceed(request);
-//            }
-//
-//        };
-//    }
+    public OkHttpClient solveHttps(OkHttpClient sClient) {
+        SSLContext sc = null;
+        try {
+            sc = SSLContext.getInstance("SSL");
+            sc.init(null, new TrustManager[]{new TrustManager()}, new SecureRandom());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
+        HostnameVerifier hv1 = new HostnameVerifier() {
+            @Override
+            public boolean verify(String hostname, SSLSession session) {
+                return true;
+            }
+        };
 
-	public OkHttpClient solveHttps(OkHttpClient sClient) {
-		SSLContext sc = null;
-		try {
-			sc = SSLContext.getInstance("SSL");
-			sc.init(null, new TrustManager[]{new X509TrustManager() {
-				@Override
-				public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) throws java.security.cert.CertificateException {
+        String workerClassName = "okhttp3.OkHttpClient";
+        try {
+            Class workerClass = Class.forName(workerClassName);
+            Field hostnameVerifier = workerClass.getDeclaredField("hostnameVerifier");
+            hostnameVerifier.setAccessible(true);
+            hostnameVerifier.set(sClient, hv1);
 
-				}
+            Field sslSocketFactory = workerClass.getDeclaredField("sslSocketFactory");
+            sslSocketFactory.setAccessible(true);
+            sslSocketFactory.set(sClient, sc.getSocketFactory());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return sClient;
+    }
 
-				@Override
-				public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) throws java.security.cert.CertificateException {
+    private static class TrustManager implements X509TrustManager {
 
-				}
+        @Override
+        public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
 
-				@Override
-				public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-					return null;
-				}
-			}}, new SecureRandom());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+        }
 
-		HostnameVerifier hv1 = new HostnameVerifier() {
-			@Override
-			public boolean verify(String hostname, SSLSession session) {
-				return true;
-			}
-		};
+        @Override
+        public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
 
-		String workerClassName = "okhttp3.OkHttpClient";
-		try {
-			Class workerClass = Class.forName(workerClassName);
-			Field hostnameVerifier = workerClass.getDeclaredField("hostnameVerifier");
-			hostnameVerifier.setAccessible(true);
-			hostnameVerifier.set(sClient, hv1);
+        }
 
-			Field sslSocketFactory = workerClass.getDeclaredField("sslSocketFactory");
-			sslSocketFactory.setAccessible(true);
-			sslSocketFactory.set(sClient, sc.getSocketFactory());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return sClient;
-	}
+        @Override
+        public X509Certificate[] getAcceptedIssuers() {
+            /*return new X509Certificate[0]*/
+            return null;
+        }
+    }
+
+    private static class MyLogger implements HttpLoggingInterceptor.Logger {
+
+        @Override
+        public void log(String message) {
+            if (BuildConfig.DEBUG) {
+                if (message.startsWith("{") && message.endsWith("}")) {
+                    LTUtils.d(">>>:\n" + message + "\n");
+                } else {
+                    LTUtils.d(message);
+                }
+            }
+        }
+    }
+
+    private static class MyHeaderInterceptor implements Interceptor {
+        @Override
+        public Response intercept(Chain chain) throws IOException {
+            Request original = chain.request();
+            Request request = original.newBuilder()
+                    ////版本号
+                    //.header("v", "AppUtils.getVersionCode(BaseApplication.getInstance())")
+                    ////系统版本号
+                    //.header("sv", Build.MANUFACTURER + " " + Build.VERSION.SDK)
+                    ////2=android
+                    .header("type", "2")
+                    .method(original.method(), original.body())
+                    .build();
+            return chain.proceed(request);
+        }
+    }
 }
